3e4402d7e5527993228db182d21b3bab150835ed
ofthekings12 <mikkode212@gmail.com>
2021-11-24 01:29:55 +0000

    implemented assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+      return false;
+    }
+  }
+
+  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  return true;
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 5]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
