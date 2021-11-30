6423cec4d8b021f2ab63f8a543d4c42db26a687e
ofthekings12 <mikkode212@gmail.com>
2021-11-30 01:37:37 +0000

    implemented object shorthand

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,4 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+"Lighthouse Labs"const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1, 2, 3], [1, 2, 5]);
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,25 +1,4 @@
-const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
-
-// assertEqual()
-// describe("#assertEqual", () => {
-//   it("returns true if they are equal", () => {
-//     assert.deepEqual(("Lighthouse Labs", "Bootcamp"));
-//   });
-//   it("returns true if they are equal", () => {
-//     assert.deepEqual(("1", "1"));
-//   });
-// });
-
-// describe("#assertEqual", () => {
-//   it("returns false if not equal", () => {
-//     assert.strictEqual(assertEqual(["Lighthouse Labs"]), "Bootcamp");
-//   });
-//   it("returns true if equal", () => {
-//     assert.strictEqual(assertEqual(['1']), '1'); 
-//   });
-
-// });
 
 
 // TEST CODE
